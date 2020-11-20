54aebfb6a904bf746a34a8d3f247a80d31f27640
Alexander <37797229+TheHasagi@users.noreply.github.com>
2020-11-20 12:52:21 +0300

    Update testingtest.js

--- testingtest.js
+++ testingtest.js
@@ -16,15 +16,55 @@
 // @grant        GM_info
 // ==/UserScript==
 
-<script>
+/* 
+ * This userscript is made for explain AdguardTeam/AdguardForAndroid#3656
+ * You should follows step to reproduce that described on issue that I wrote on.
+ */
 
-...
-.
-.
-.
-.
-...
+//@ts-check
+(async () => {
+    let mainDiv = document.querySelector("div")
+    if (!mainDiv) return;
+    
+    
+    /** @type string */
+    //@ts-expect-error (No type definition for GreaseMonkey sets on this workspace)
+    let scriptVersion = GM.info.script.version
+    let versionP = document.createElement("p")
+    versionP.innerHTML = `Script Version: ${scriptVersion}`
 
-! just a comment
+     /** @type string */
+    //@ts-expect-error (No type definition for GreaseMonkey sets on this workspace)
+    let scriptValue = await GM.getValue("value", "InitialValue")
+    let valueP = document.createElement("p")
+    valueP.innerHTML = `Script Value: ${scriptValue}`
 
-</script>
+    let inputP = document.createElement("p")
+    /** @type HTMLInputElement */
+    let inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "text");
+    let buttonElement = document.createElement("input");
+    buttonElement.setAttribute("type", "button");
+    buttonElement.value = "Submit"
+    let statusElement = document.createElement("p")
+
+    buttonElement.addEventListener("click", async () => {
+        //@ts-expect-error (No type definition for GreaseMonkey sets on this workspace)
+        await GM.setValue("value", inputElement.value)
+
+        //@ts-expect-error (No type definition for GreaseMonkey sets on this workspace)
+        let savedValue = await GM.getValue("value", "InitialValue")
+
+        valueP.innerHTML = `Script Value: ${savedValue}`
+        statusElement.innerHTML = "The Value is saved. Loaded with script again: " + savedValue
+    })
+
+    inputP.appendChild(inputElement)
+    inputP.appendChild(buttonElement)
+
+
+    mainDiv.appendChild(versionP)
+    mainDiv.appendChild(valueP)
+    mainDiv.appendChild(inputP)
+    mainDiv.appendChild(statusElement)
+})();
