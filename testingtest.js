d393755f90ab5fbb8c81aa87721849061d66f68c
Alexander <37797229+TheHasagi@users.noreply.github.com>
2020-11-20 12:51:28 +0300

    Update testingtest.js

--- testingtest.js
+++ testingtest.js
@@ -1,5 +1,21 @@
-lala lal al 
-! version : 2.0
+// ==UserScript==
+// @name         AdguardTeam/AdguardForAndroid#3656
+// @description  The userscript storage wipes out when update. on Adgaurd For Android
+// @version      0.0.2
+// @include      https://example.com/*
+// @include      https://example.net/*
+// @include      http://example.com/*
+// @include      http://example.net/*
+// @run-at       document-end
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant        GM.getValue
+// @grant        GM_getValue
+// @grant        GM.setValue
+// @grant        GM_setValue
+// @grant        GM.info
+// @grant        GM_info
+// ==/UserScript==
+
 <script>
 
 ...
